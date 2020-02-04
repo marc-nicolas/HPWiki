d1f5d83d24b63310bf4f7e78c91f19ffb4e11c87
marc-nicolas <60380828+marc-nicolas@users.noreply.github.com>
2020-02-04 09:39:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hp_wiki/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hp_wiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
