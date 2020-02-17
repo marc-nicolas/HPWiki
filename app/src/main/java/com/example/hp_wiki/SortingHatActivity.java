6045885f0c6e30e102e09b6a8511290671a6126e
VanessaEberhard <43634679+VanessaEberhard@users.noreply.github.com>
2020-02-17 14:40:17 +0100

    make little changes at the sorting hat activity

--- app/src/main/java/com/example/hp_wiki/SortingHatActivity.java
+++ app/src/main/java/com/example/hp_wiki/SortingHatActivity.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.text.LineBreaker;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
